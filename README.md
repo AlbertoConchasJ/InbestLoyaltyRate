1413354b438519fa56ef79ccd02160df73353b03
AlbertoConchasJ <alberto.conchas@inbest.me>
2018-03-08 16:15:32 -0600

    initialization

---
+++ README.md
@@ -0,0 +1,12 @@
+# Serverless Demo
+
+This application uses AWS to generate a complete serverless website with the next tools:
+1. Deploy static website using S3
+2. Login with cognito
+3. Use Lambda and Api gateway to do dynamic calls 
+4. Consult dynamo DB with lambda
+ 
+
+![Architecture](images/arc.png)
+
+
---
+++ static web page/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_iOFpTHmtq', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1rp4t3rksdvur4gp09vpoavnb4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://0hbrqj9299.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
